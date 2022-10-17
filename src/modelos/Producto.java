36242fa53b778ab7af6e27b0dbeca0c87577ecf3
Lucas <lucasdelellis2002@gmail.com>
2022-10-17 14:50:00 -0300

    Clase producto.

---
+++ src/modelos/Producto.java
@@ -0,0 +1,9 @@
+package modelos;
+
+public class Producto {
+    private int id;
+    private String nombre;
+    private double precioCosto;
+    private double precioVenta;
+    private int stock;
+}
