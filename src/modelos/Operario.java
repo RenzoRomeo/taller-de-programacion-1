940e5963b06977aab0a150dcb78dee7c0b0c9a27
Renzo Romeo <renzoromeo44@gmail.com>
2022-10-17 14:51:10 -0300

    Operario

---
+++ src/modelos/Operario.java
@@ -0,0 +1,17 @@
+package modelos;
+
+public class Operario {
+    private String nombre;
+    private String apellido;
+    private String nombreUsuario;
+    private String contrasenia;
+    private boolean activo;
+
+    private void establecerEstadoMozo(Mozo mozo, Estado estado) {
+
+    }
+
+    private void asignarMesa(Mozo mozo, Mesa mesa) {
+
+    }
+}
