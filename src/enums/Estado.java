e34dd8418740cf01b32377ea8e3f2e8c82c0d344
TeoRamosKees <teoramites@gmail.com>
2022-10-19 11:28:55 -0300

    Clase mozo

---
+++ src/enums/Estado.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum Estado {
+    AUSENTE,
+    ACTIVO,
+    DE_FRANCO
+}
---
+++ src/modelo/Mozo.java
@@ -0,0 +1,24 @@
+package modelo;
+
+import enums.Estado;
+
+import java.util.Date;
+
+public class Mozo {
+    private String nombre;
+    private String apellido;
+    private Date fechaNacimiento;
+    private int hijosACargo;
+    private Estado estado;
+
+    public Mozo(String nombre, String apellido, Date fechaNacimiento, int hijosACargo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNacimiento = fechaNacimiento;
+            this.hijosACargo = hijosACargo;
+            this.estado = Estado.ACTIVO;
+        }
+
+}
+
