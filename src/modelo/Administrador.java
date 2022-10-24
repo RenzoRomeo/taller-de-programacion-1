8da44b55f5d204486b465d2d2f0c623c654c2bd2
Teoramites <teoramites@gmail.com>
2022-10-24 17:59:02 -0300

    Cambio varios

--- src/modelo/Administrador.java
+++ src/modelo/Administrador.java
@@ -4,7 +4,60 @@ public class Administrador extends Operario {
     private boolean establecioContrasena;
 
     public Administrador(String nombre, String apellido, String nombreUsuario, String contrasenia, boolean activo, boolean establecioContrasena) {
-        super(nombre, apellido, nombreUsuario, contrasenia, activo);
+        super(nombre, apellido, nombreUsuario, contrasenia);
         this.establecioContrasena = establecioContrasena;
     }
+
+    public void agregarOperario(Operario operario) {
+        Sistema s = Sistema.getInstance();
+        s.agregarOperario(operario);
+    }
+    public void eliminarOperario(Operario operario) {
+        Sistema s = Sistema.getInstance();
+        s.eliminarOperario(operario);
+    }
+    public void modificarOperario(Operario operario) {
+        Sistema s = Sistema.getInstance();
+        s.modificarOperario(operario);
+    }
+
+    public void agregarMozo(Mozo mozo) {
+        Sistema s = Sistema.getInstance();
+        s.agregarMozo(mozo);
+    }
+    public void eliminarMozo(Mozo mozo) {
+        Sistema s = Sistema.getInstance();
+        s.eliminarMozo(mozo);
+    }
+    public void modificarMozo(Mozo mozo) {
+        Sistema s = Sistema.getInstance();
+        s.modificarMozo(mozo);
+    }
+
+    public void agregarProducto(Producto producto) {
+        Sistema s = Sistema.getInstance();
+        s.agregarProducto(producto);
+    }
+    public void eliminarProducto(Producto producto) {
+        Sistema s = Sistema.getInstance();
+        s.eliminarProducto(producto);
+    }
+    public void modificarProducto(Producto producto) {
+        Sistema s = Sistema.getInstance();
+        s.modificarProducto(producto);
+    }
+
+    public void agregarMesa(Mesa mesa) {
+        Sistema s = Sistema.getInstance();
+        s.agregarMesa(mesa);
+    }
+    public void eliminarMesa(Mesa mesa) {
+        Sistema s = Sistema.getInstance();
+        s.eliminarMesa(mesa);
+    }
+    public void modificarMesa(Mesa mesa) {
+        Sistema s = Sistema.getInstance();
+        s.modificarMesa(mesa);
+    }
+
 }
