136119e0561e1468b2d857083058123610bd5336
Lucas <lucasdelellis2002@gmail.com>
2022-10-24 17:03:48 -0300

    Invariantes y constructor de operario.

--- src/modelos/Operario.java
+++ src/modelos/Operario.java
@@ -3,12 +3,76 @@ package modelos;
 import excepciones.ContraseniaIncorrectaException;
 import excepciones.UsuarioInactivoException;
 
+/**
+ * Clase que representa a un operario del sistema.
+ * <b>Inv:</b>
+ * nombre != null
+ * nombre != ""
+ * nombre permance constante
+ * apellido != null
+ * apellido != ""
+ * apellido permance constante
+ * nombreUsuario != null
+ * nombreUsuario != ""
+ * contrasenia != null
+ * contrasenia.length() >= 6
+ * contrasenia.length() <= 12
+ * contrasenia contiene al menos un numero
+ * contrasenia contiene al menos una letra mayuscula
+ */
 public class Operario {
     private String nombre;
     private String apellido;
     private String nombreUsuario;
     private String contrasenia;
     private boolean activo;
+
+    /**
+     * Constructor de la clase Operario.
+     * @param nombre Nombre del operario.
+     * @param apellido Apellido del operario.
+     * @param nombreUsuario Nombre de usuario del operario.
+     * @param contrasenia Contraseña del operario.
+     * <b>Pre:</b>
+     * nombre != null
+     * nombre != ""
+     * apellido != null
+     * apellido != ""
+     * nombreUsuario != null
+     * nombreUsuario != ""
+     * contrasenia != null
+     * contrasenia.length() >= 6
+     * contrasenia.length() <= 12
+     * contrasenia contiene al menos un numero
+     * contrasenia contiene al menos una letra mayuscula
+     * <b>Post:</b>
+     * Se crea un nuevo operario con los datos ingresados y con el estado activo.
+     */
+    public Operario(String nombre, String apellido, String nombreUsuario, String contrasenia) {
+        assert nombre != null;
+        assert nombre != "";
+        assert apellido != null;
+        assert apellido != "";
+        assert nombreUsuario != null;
+        assert nombreUsuario != "";
+        assert contrasenia != null;
+        assert contrasenia.length() >= 6;
+        assert contrasenia.length() <= 12;
+        assert contrasenia.matches(".*[0-9].*");
+        assert contrasenia.matches(".*[A-Z].*");
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+        this.activo = true;
+
+        assert this.nombre == nombre;
+        assert this.apellido == apellido;
+        assert this.nombreUsuario == nombreUsuario;
+        assert this.contrasenia == contrasenia;
+        assert this.activo == true;
+    }
 
     public void establecerEstadoMozo(Mozo mozo, Estado estado) {
 
