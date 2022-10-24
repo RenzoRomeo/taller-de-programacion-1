8927e3180508133a621d3ed1974c78a7760ba184
Lucas <lucasdelellis2002@gmail.com>
2022-10-24 16:31:09 -0300

    Iniciar sesion para operario.

--- src/modelos/Operario.java
+++ src/modelos/Operario.java
@@ -1,5 +1,8 @@
 package modelos;
 
+import excepciones.ContraseniaIncorrectaException;
+import excepciones.UsuarioInactivoException;
+
 public class Operario {
     private String nombre;
     private String apellido;
@@ -7,11 +10,53 @@ public class Operario {
     private String contrasenia;
     private boolean activo;
 
-    private void establecerEstadoMozo(Mozo mozo, Estado estado) {
+    public void establecerEstadoMozo(Mozo mozo, Estado estado) {
 
     }
 
-    private void asignarMesa(Mozo mozo, Mesa mesa) {
+    public void asignarMesa(Mozo mozo, Mesa mesa) {
 
+    }
+
+    /**
+     * Inicia sesion a un operario dentro del sistema.
+     * @param contrasenia La contraseña del operario.
+     * @throws UsuarioInactivoException Si el usuario se encuentra inactivo.
+     * @throws ContraseniaIncorrectaException Si la contraseña ingresada es incorrecta.
+     */
+    public void iniciarSesion(String contrasenia) throws UsuarioInactivoException, ContraseniaIncorrectaException {
+        if(!activo) {
+            throw new UsuarioInactivoException(this);
+        }
+
+        if(!this.contrasenia.equals(contrasenia)) {
+            throw new ContraseniaIncorrectaException();
+        }
+
+        // Acciones si se loguea bien
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
     }
 }
