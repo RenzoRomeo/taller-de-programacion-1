97ae59f057512c876cde81a991120e210b04f49d
Renzo Romeo <renzoromeo44@gmail.com>
2022-10-25 17:06:25 -0300

    Constructor comanda.

--- src/modelos/Comanda.java
+++ src/modelos/Comanda.java
@@ -1,9 +1,15 @@
 package modelos;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Comanda {
     private Date fecha;
     private List<Pedido> pedidos;
+
+    public Comanda() {
+        fecha = new Date();
+        pedidos = new ArrayList<Pedido>();
+    }
 }
