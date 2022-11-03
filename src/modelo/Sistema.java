4a53b5ede665722b4a4469dce28ec4902bd2f1c0
Teoramites <teoramites@gmail.com>
2022-11-03 15:53:33 -0300

    Aserciones clase Sistema

--- src/modelo/Sistema.java
+++ src/modelo/Sistema.java
@@ -76,34 +76,102 @@ public class Sistema {
         //TODO
     }
 
+    /**
+     * Agrega mozo al sistema
+     * @param mozo
+     * <b>Pre:</b>
+     * mozo != null
+     *
+     */
     public void agregarMozo(Mozo mozo) {
+        assert mozo != null;
+
         mozos.add(mozo);
     }
+
+    /**
+     * Elimina mozo del sistema
+     * @param mozo
+     * <b>Pre:</b>
+     * mozo != null
+     *
+     */
     public void eliminarMozo(Mozo mozo) {
+        assert mozo != null;
+
         mozos.remove(mozo);
     }
     public void modificarMozo(Mozo mozo) {
         //TODO
     }
 
+    /**
+     * establece estado mozo
+     * @param mozo
+     * @param estado
+     * <b>Pre:</b>
+     * mozo != null
+     * estado != null
+     *
+     */
     public void establecerEstadoMozo(Mozo mozo, Estado estado) {
+        assert mozo != null;
+        assert estado != null;
+
         mozo.setEstado(estado);
     }
 
+    /**
+     * Agrega mesa al sistema
+     * @param mesa
+     * <b>Pre:</b>
+     * mesa != null
+     *
+     */
     public void agregarMesa(Mesa mesa) {
+        assert mesa != null;
+
         mesas.add(mesa);
     }
+
+    /**
+     * Elimina mesa del sistema
+     * @param mesa
+     * <b>Pre:</b>
+     * mesa != null
+     *
+     */
     public void eliminarMesa(Mesa mesa) {
+        assert mesa != null;
+
         mesas.remove(mesa);
     }
     public void modificarMesa(Mesa mesa) {
         //TODO
     }
 
+    /**
+     * Agrega producto al sistema
+     * @param producto
+     * <b>Pre:</b>
+     * producto != null
+     *
+     */
     public void agregarProducto(Producto producto) {
+        assert producto != null;
+
         productos.add(producto);
     }
+
+    /**
+     * Elimina producto del sistema
+     * @param producto
+     * <b>Pre:</b>
+     * producto != null
+     *
+     */
     public void eliminarProducto(Producto producto) {
+        assert producto != null;
         productos.remove(producto);
     }
     public void modificarProducto(Producto producto) {
@@ -112,17 +180,65 @@ public class Sistema {
 
     //La comanda va estar abierta siempre y cuando exista
     //Se crea con un unico producto y se le van agregando pedidos
+
+    /**
+     *
+     * @param mesa
+     * @param producto
+     * @param cantidad
+     *
+     * <b>Pre:</b>
+     * mesa != null
+     * producto != null
+     * cantidad > 0
+     *
+     */
     public void crearComanda(Mesa mesa, Producto producto, int cantidad) {
+        assert mesa != null;
+        assert producto != null;
+        assert cantidad > 0;
+
         Comanda comanda = new Comanda(producto, cantidad);
         comandas.put(mesa, comanda);
     }
 
     //Se agrega de a un producto a la comanda
+
+    /**
+     *
+     * @param p
+     * @param cantidad
+     * @param mesa
+     *
+     * <b>Pre:</b>
+     * p != null
+     * cantidad > 0
+     * mesa != null
+     *
+     */
     public void agregarPedido(Producto p, int cantidad, Mesa mesa) {
+        assert p != null;
+        assert cantidad > 0;
+        assert mesa != null;
+
         Comanda comanda = comandas.get(mesa);
         comanda.agregarPedido(p, cantidad);
     }
+
+    /**
+     *
+     * @param mesa
+     * @param formaDePago
+     *
+     * <b>Pre:</b>
+     * mesa != null
+     * formaDePago != null
+     *
+     */
     public void cerrarComanda(Mesa mesa, FormaDePago formaDePago) {
+        assert mesa != null;
+        assert formaDePago != null;
+
         Comanda comanda = comandas.get(mesa);
         comanda.cerrarComanda(formaDePago);
         comandas.remove(mesa);
