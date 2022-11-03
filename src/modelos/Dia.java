5223987f958ebcb0d01c0692e65162f011ec9666
Renzo Romeo <renzoromeo44@gmail.com>
2022-11-03 17:47:32 -0300

    Dia actual.

--- src/modelos/Dia.java
+++ src/modelos/Dia.java
@@ -1,5 +1,7 @@
 package modelos;
 
+import java.util.Calendar;
+
 public enum Dia {
     LUNES,
     MARTES,
@@ -7,5 +9,35 @@ public enum Dia {
     JUEVES,
     VIERNES,
     SABADO,
-    DOMINGO
+    DOMINGO;
+
+    public static Dia getDiaActual() {
+        Calendar c = Calendar.getInstance();
+        int dia = c.get(Calendar.DAY_OF_WEEK);
+        Dia diaActual = null;
+        switch (dia) {
+            case Calendar.MONDAY:
+                diaActual = Dia.LUNES;
+                break;
+            case Calendar.TUESDAY:
+                diaActual = Dia.MARTES;
+                break;
+            case Calendar.WEDNESDAY:
+                diaActual = Dia.MIERCOLES;
+                break;
+            case Calendar.THURSDAY:
+                diaActual = Dia.JUEVES;
+                break;
+            case Calendar.FRIDAY:
+                diaActual = Dia.VIERNES;
+                break;
+            case Calendar.SATURDAY:
+                diaActual = Dia.SABADO;
+                break;
+            case Calendar.SUNDAY:
+                diaActual = Dia.DOMINGO;
+                break;
+        }
+        return diaActual;
+    }
 }
