334a7e0744bd462a29b249bedfba6a9ebb0cf107
Renzo Romeo <renzoromeo44@gmail.com>
2022-11-04 16:22:15 -0300

    Controlador principal para persistir/despersistir el sistema y establecer controlador actual.

---
+++ src/controladores/Principal.java
@@ -0,0 +1,31 @@
+package controladores;
+
+// TODO: Cambiar nombre de esta clase, no sabía como ponerle.
+public class Principal {
+    private static Principal instancia = null;
+    private Controller controladorActual;
+
+    private Principal() {
+
+    }
+
+    public static Principal getInstancia() {
+        if (instancia == null) {
+            instancia = new Principal();
+        }
+
+        return instancia;
+    }
+
+    public void setControladorActual(Controller controlador) {
+        controladorActual = controlador;
+    }
+
+    public void persistirSistema() {
+
+    }
+
+    public void despersistirSistema() {
+
+    }
+}
