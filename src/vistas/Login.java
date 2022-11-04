946f45e7ae5335d918439a8c92fb93a8015c1c24
Lucas <lucasdelellis2002@gmail.com>
2022-11-04 13:52:31 -0300

    Login screen.

---
+++ src/vistas/Login.java
@@ -0,0 +1,12 @@
+package vistas;
+
+import javax.swing.*;
+
+public class Login {
+    private JPanel contentPane;
+    private JPanel principal;
+    private JPanel titulo;
+    private JPanel botones;
+    private JTextField tituloText;
+
+}
