add0f6171c5f84941e55ea96cf738735903b539b
Lucas <lucasdelellis2002@gmail.com>
2022-11-06 18:22:11 -0300

    Window listener en IVista.

--- src/controladores/Controller.java
+++ src/controladores/Controller.java
@@ -3,12 +3,14 @@ package controladores;
 import vistas.IVista;
 
 import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
 
-public abstract class Controller<E extends IVista> implements ActionListener {
+public abstract class Controller<E extends IVista> implements ActionListener, WindowListener {
     protected E vista;
 
     public Controller(E vista) {
         this.vista = vista;
         this.vista.setActionListener(this);
+        this.vista.setWindowListener(this);
     }
 }
--- src/vistas/IVista.java
+++ src/vistas/IVista.java
@@ -1,7 +1,10 @@
 package vistas;
 
 import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
 
 public interface IVista {
     void setActionListener(ActionListener actionListener);
+
+    void setWindowListener(WindowListener windowListener);
 }
