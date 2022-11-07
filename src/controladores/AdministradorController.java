15c321d83e5ca126ee12f8503155300155cfd5eb
Renzo Romeo <renzoromeo44@gmail.com>
2022-11-06 22:59:40 -0300

    Vista administrador.

--- src/controladores/AdministradorController.java
+++ src/controladores/AdministradorController.java
@@ -11,6 +11,20 @@ public class AdministradorController extends Controller<Administrador> {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
 
+        if (cmd.equalsIgnoreCase("gestionarNegocio")) {
+            Principal.getInstancia().setControladorActual(new GestionarNegocioController());
+        } else if (cmd.equalsIgnoreCase("gestionarOperarios")) {
+            Principal.getInstancia().setControladorActual(new GestionarOperariosController());
+        } else if (cmd.equalsIgnoreCase("gestionarProductos")) {
+            Principal.getInstancia().setControladorActual(new GestionarProductosController());
+        } else if (cmd.equalsIgnoreCase("cerrarSesion")) {
+            Principal.getInstancia().setControladorActual(new LoginController());
+        } else if (cmd.equalsIgnoreCase("gestionarMesas")) {
+            Principal.getInstancia().setControladorActual(new GestionarMesasController());
+        } else if (cmd.equalsIgnoreCase("gestionarMozos")) {
+            Principal.getInstancia().setControladorActual(new GestionarMozosController());
+        }
     }
 }
