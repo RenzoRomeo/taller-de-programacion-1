3b4a6920428a8de7fb771d0e613963bf3f0da359
Renzo Romeo <renzoromeo44@gmail.com>
2022-11-06 23:02:00 -0300

    Controllers creados.

---
+++ src/controladores/GestionarMozosController.java
@@ -0,0 +1,15 @@
+package controladores;
+
+import java.awt.event.ActionEvent;
+
+public class GestionarMozosController extends Controller<GestionarMozos> {
+
+    public GestionarMozosController() {
+        super(new GestionarMozos());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
