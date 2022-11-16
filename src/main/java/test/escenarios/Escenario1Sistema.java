6312ea9ee594b2b8a96fad577a535c42d6846148
Renzo Romeo <renzoromeo44@gmail.com>
2022-11-16 11:48:26 -0300

    Escenario 1 con mozo.

--- src/main/java/test/escenarios/Escenario1Sistema.java
+++ src/main/java/test/escenarios/Escenario1Sistema.java
@@ -1,22 +1,32 @@
 package test.escenarios;
 
+import excepciones.MozoYaExistenteException;
 import excepciones.NombreDeUsuarioNoDisponibleException;
+import modelo.Mozo;
 import modelo.Operario;
 import modelo.Sistema;
 
+import java.util.Date;
+
 public class Escenario1Sistema {
-    private static Operario juan;
-    private static Operario jose;
+    private static Operario operarioJuan;
+    private static Operario operarioJose;
+
+    private static Mozo mozoAlberto;
 
     public static void setUp() {
         Sistema sistema = Sistema.getInstance();
-        juan = new Operario("Juan", "Perez", "Juan", "Juan123");
-        jose = new Operario("Jose", "Perez", "Jose", "Jose123");
-        jose.setActivo(false);
+        operarioJuan = new Operario("Juan", "Perez", "Juan", "Juan123");
+        operarioJose = new Operario("Jose", "Perez", "Jose", "Jose123");
+        operarioJose.setActivo(false);
+
+        mozoAlberto = new Mozo("Alberto", "Perez", new Date(1995, 5, 10), 0);
+
         try {
-            sistema.agregarOperario(juan);
-            sistema.agregarOperario(jose);
-        } catch (NombreDeUsuarioNoDisponibleException e) {
+            sistema.agregarOperario(operarioJuan);
+            sistema.agregarOperario(operarioJose);
+            sistema.agregarMozo(mozoAlberto);
+        } catch (NombreDeUsuarioNoDisponibleException | MozoYaExistenteException e) {
 
         }
     }
@@ -24,13 +34,18 @@ public class Escenario1Sistema {
     public static void tearDown() {
         Sistema sistema = Sistema.getInstance();
         sistema.getOperarios().clear();
+        sistema.getMozos().clear();
     }
 
-    public static Operario getJuan() {
-        return juan;
+    public static Operario getOperarioJuan() {
+        return operarioJuan;
     }
 
-    public static Operario getJose() {
-        return jose;
+    public static Operario getOperarioJose() {
+        return operarioJose;
+    }
+
+    public static Mozo getMozoAlberto() {
+        return mozoAlberto;
     }
 }
