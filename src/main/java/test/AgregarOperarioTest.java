b695c43f2dacfab8706df985dee125f843f9609a
Lucas <lucasdelellis2002@gmail.com>
2022-11-16 11:38:06 -0300

    Correccion de test en agregar operario.

--- src/main/java/test/AgregarOperarioTest.java
+++ src/main/java/test/AgregarOperarioTest.java
@@ -1,25 +1,37 @@
 package test;
 
-import excepciones.NombreDeUsuarioNoDisponibleException;
-import modelo.Operario;
-import modelo.Sistema;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import excepciones.NombreDeUsuarioNoDisponibleException;
+import modelo.Operario;
+import modelo.Sistema;
 
 class AgregarOperarioTest {
     private static Sistema sistema;
 
     @BeforeAll
     static void setUp() {
+    	Escenario1Sistema.setUp();
         sistema = Sistema.getInstance();
+    }
+    
+    @AfterAll
+    static void tearDown() {
+    	Escenario1Sistema.tearDown();
     }
 
     @Test
     void operarioAgregado() {
         try {
-            sistema.agregarOperario(new Operario("Juan", "Perez", "Juan", "Juan123"));
+        	Operario operarioAgregado = new Operario("Jorge", "Perez", "Jorge", "Jorge123");
+            sistema.agregarOperario(operarioAgregado);
+            Operario operarioSistema = sistema.getOperarios().get("Jorge");
+            assertEquals(operarioAgregado, operarioSistema, "El operario deberia haberse agregado correctamente");
         } catch (NombreDeUsuarioNoDisponibleException e) {
             fail("El operario deberia haberse agregado correctamente");
         }
@@ -28,8 +40,7 @@ class AgregarOperarioTest {
     @Test
     void nombreDeUsuarioNoDisponible() {
         try {
-            sistema.agregarOperario(new Operario("Jose", "Perez", "Jose", "Jose123"));
-            sistema.agregarOperario(new Operario("Jose", "Perez", "Jose", "Jose123"));
+            sistema.agregarOperario(new Operario("Juan Martin", "Fernandez", "Juan", "Password123"));
             fail("El nombre de usuario no deberia estar disponible");
         } catch (NombreDeUsuarioNoDisponibleException e) {
 
